87cb4710ea1f175763b54c55250e24587c36b9db
Matti Armston <matti.armston@gmail.com>
2022-05-02 17:39:16 +0100

    Car rotates on the spot
    After 2 days I have finally gotten the car to rotate on the spot. I
    eventually realised that I could use the center positions from before
    and after the rotation to see how much the car's image had moved during
    the rotation. I had to do this because pygame will always rotate an
    image around the top left hand corner. I could then use the difference
    in x and y positions to find an offset, which I could then apply to the
    car's x and y positions to move them back to where they were before the
    rotation.
    The reason I did this over just drawing the image to the screen in the
    rotate function was to keep the simple drawing logic that I currently
    use. This may also make things such as collision detection easier as the
    x and y coordinates of the player will be correct.

--- player.py
+++ player.py
@@ -1,20 +1,28 @@
+import pygame
 import logging
 
 from gameObject import GameObject
 
 class Player(GameObject):
     def __init__(self, image, game):
-        super().__init__(50, 50, 1157 / 4, 470 / 4, image, game)
+        super().__init__(500, 500, 1157 / 4, 470 / 4, image, game)
+        self.originalImage = self.image
         self.speedX = 0
         self.speedY = 0
         self.accelX = 1
         self.accelY = 1
         self.maxSpeedX = 50
         self.maxSpeedY = 50
+        self.direction = 0
+        self.turningSpeed = 0
+        self.turningAccel = 5
+        self.maxTurningSpeed = 50
+        pygame.transform.rotate(self.image, 90)
 
     def move(self, keys, keyBinds):
         self.updateSpeed(keys, keyBinds)
         self.constrainSpeed()
+        self.turn()
         self.moveSelf()
 
     def updateSpeed(self, keys, keyBinds):
@@ -22,10 +30,12 @@ class Player(GameObject):
             for key in keyBinds[direction]:
                 if keys[key]:
                     if direction == "left":
-                        self.speedX -= self.accelX
+                        #self.speedX -= self.accelX
+                        self.direction += self.turningAccel
                         logging.info("left  key pressed")
                     if direction == "right":
-                        self.speedX += self.accelX
+                        #self.speedX += self.accelX
+                        self.direction -= self.turningAccel
                         logging.info("right key pressed")
                     if direction == "up":
                         self.speedY -= self.accelY
@@ -46,6 +56,15 @@ class Player(GameObject):
 
     def friction(self):
         return
+
+    def turn(self):
+        originalCenter = self.image.get_rect().center
+        self.image = pygame.transform.rotate(self.originalImage, self.direction)
+        newCenter = self.image.get_rect().center
+        offsetX = originalCenter[0] - newCenter[0]
+        offsetY = originalCenter[1] - newCenter[1]
+        self.x += offsetX
+        self.y += offsetY
 
     def moveSelf(self):
         self.x += self.speedX
